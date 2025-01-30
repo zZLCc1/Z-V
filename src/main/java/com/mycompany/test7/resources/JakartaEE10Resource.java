55a93b7efb19fe9e1649aab92631cca36a0e4ee1
zZLCc1 <zahidc88@icloud.com>
2025-01-30 01:34:09 -0700

    Add files via upload

---
+++ src/main/java/com/mycompany/test7/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.mycompany.test7.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
